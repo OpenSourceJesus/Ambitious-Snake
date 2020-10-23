1c896c9f3c13810e28d8c8f4c2b05b1fd2180293
Gilead Cosman <gileadcosman@gmail.com>
2020-10-23 11:15:13 -0700

    Improved code

--- Assets/Standard Assets/Scripts/SphereAnimation.cs
+++ Assets/Standard Assets/Scripts/SphereAnimation.cs
@@ -1,41 +1,56 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 using Extensions;
+using AmbitiousSnake;
 
-public class SphereAnimation : SingletonMonoBehaviour<SphereAnimation>
+public class SphereAnimation : SingletonMonoBehaviour<SphereAnimation>, IUpdatable
 {
+	public bool PauseWhileUnfocused
+	{
+		get
+		{
+			return true;
+		}
+	}
+	public Transform trs;
 	public float missileMapBounds;
-	Vector2 missilePos;
-	Vector2 missileDest;
-	Vector2 missileVel;
 	public float missileTurnRate;
 	public float missileSpeed;
-	float missileDistToDest;
+	Vector2 missilePosition;
+	Vector2 missileDestination;
+	Vector2 missileVelocity;
+	float missileDistToDestSqr;
 	bool missileHasReducedDistToDest;
 	
 	public override void Awake ()
 	{
 		base.Awake ();
-		missileVel = Random.insideUnitCircle.normalized * missileSpeed;
-		missileDest = Random.insideUnitCircle * missileMapBounds;
+		missileVelocity = Random.insideUnitCircle.normalized * missileSpeed;
+		missileDestination = Random.insideUnitCircle * missileMapBounds;
+		GameManager.updatables = GameManager.updatables.Add(this);
 	}
 
-	public virtual void Update ()
+	void OnDestroy ()
 	{
-		missileDistToDest = Vector2.Distance(missilePos, missileDest);
-		float missileIdealTurnAmount = Vector2.Angle(missileVel, missileDest - missilePos);
+		GameManager.updatables = GameManager.updatables.Remove(this);
+	}
+
+	public virtual void DoUpdate ()
+	{
+		float prevMissileDistToDestSqr = (missilePosition - missileDestination).sqrMagnitude;
+		float missileIdealTurnAmount = Vector2.Angle(missileVelocity, missileDestination - missilePosition);
 		float missileTurnAmount = Mathf.Clamp(missileIdealTurnAmount, -missileTurnRate * Time.deltaTime, missileTurnRate * Time.deltaTime);
-		missileVel = VectorExtensions.Rotate(missileVel, missileTurnAmount);
-		missileVel = missileVel.normalized * missileSpeed * Time.deltaTime;
-		missilePos += missileVel;
-		if (missileHasReducedDistToDest && Vector2.Distance(missilePos, missileDest) > missileDistToDest)
+		missileVelocity = VectorExtensions.Rotate(missileVelocity, missileTurnAmount);
+		missileVelocity = missileVelocity.normalized * missileSpeed * Time.deltaTime;
+		missilePosition += missileVelocity;
+		float missileDistToDestSqr = (missilePosition - missileDestination).sqrMagnitude;
+		if (missileHasReducedDistToDest && missileDistToDestSqr > prevMissileDistToDestSqr)
 		{
-			missileDest = Random.insideUnitCircle * missileMapBounds;
+			missileDestination = Random.insideUnitCircle * missileMapBounds;
 			missileHasReducedDistToDest = false;
 		}
-		else if (Vector2.Distance(missilePos, missileDest) < missileDistToDest)
+		else if (missileDistToDestSqr < prevMissileDistToDestSqr)
 			missileHasReducedDistToDest = true;
-		transform.RotateAround(transform.position, transform.forward, missileVel.x);
-		transform.RotateAround(transform.position, transform.right, missileVel.y);
+		trs.RotateAround(trs.position, trs.forward, missileVelocity.x);
+		trs.RotateAround(trs.position, trs.right, missileVelocity.y);
 	}
 }
