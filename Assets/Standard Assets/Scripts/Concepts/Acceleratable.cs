aa8563239cf74c738407a633d2e60ae27a6d6efd
Gilead Cosman <gileadcosman@gmail.com>
2020-10-24 17:17:42 -0700

    Did stuff

--- Assets/Standard Assets/Scripts/Concepts/Acceleratable.cs
+++ Assets/Standard Assets/Scripts/Concepts/Acceleratable.cs
@@ -1,13 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using Extensions;
+using AmbitiousSnake;
 
-public class Acceleratable : MonoBehaviour
+public class Acceleratable : MonoBehaviour, IUpdatable
 {
+	public bool PauseWhileUnfocused
+	{
+		get
+		{
+			return true;
+		}
+	}
 	[HideInInspector]
 	public List<Vector2> forces = new List<Vector2>();
 	public Rigidbody2D rigid;
+
+	void OnEnable ()
+	{
+		GameManager.updatables = GameManager.updatables.Add(this);
+	}
+
+	void OnDisable ()
+	{
+		GameManager.updatables = GameManager.updatables.Remove(this);
+	}
 	
-	void FixedUpdate ()
+	public void DoUpdate ()
 	{
 		List<Vector2> appliedForces = new List<Vector2>();
 		foreach (Vector2 force in forces)
